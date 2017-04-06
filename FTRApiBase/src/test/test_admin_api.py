b8e7eabf2b0c5ac6972c5210253eabfaa3d29df4
Ubuntu <ftr@FtrApplication.jn3yhxsmid4enmmko0pk55fedg.mx.internal.cloudapp.net>
2017-04-06 02:41:16 +0000

    remove comment

--- FTRApiBase/src/test/test_admin_api.py
+++ FTRApiBase/src/test/test_admin_api.py
@@ -8,16 +8,6 @@ from flask_jsonrpc.proxy import ServiceProxy
 AZURE = "http://ftr-app.japanwest.cloudapp.azure.com:5000"
 LOCAL = "http://localhost:5000"
 
-# SERVER_URL = AZURE
-# 
-# SERVICE_PROXY = SERVER_URL + "/api"
-# 
-# server = ServiceProxy(SERVICE_PROXY)
-# cmm_index = server.cmm.index
-# 
-# r = rpc_call(cmm_index)
-# print(pformat(r))
-
 class TestCmmServiceProxy(object):
     SERVER_URL = LOCAL
 
